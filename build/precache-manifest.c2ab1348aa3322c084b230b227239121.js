83500216517d22c7c900627e67c6d8f686860287
Glavas <44177576+Glavas@users.noreply.github.com>
2019-09-27 14:06:57 +0200

    Add files via upload

---
+++ build/precache-manifest.c2ab1348aa3322c084b230b227239121.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b0ba41f3b9c31f974cb1ae93d0e63e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0280950304afb1a37fa",
+    "url": "/static/css/main.9fe86dd7.chunk.css"
+  },
+  {
+    "revision": "367a27696928b62539bc",
+    "url": "/static/js/2.dcfee1a2.chunk.js"
+  },
+  {
+    "revision": "e0280950304afb1a37fa",
+    "url": "/static/js/main.ad5abf53.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  },
+  {
+    "revision": "9f46ca128bea128763c9c4bec1bfb3ae",
+    "url": "/static/media/background-contentContainer.9f46ca12.jpg"
+  },
+  {
+    "revision": "75953fc4e8fcbf158fd49b27a485218c",
+    "url": "/static/media/purpleDust2.75953fc4.jpg"
+  }
+]);
